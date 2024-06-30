6d11b98d720f97341c31eab7943706c2b56a6bad
Sheesh <vladsolokov@gmail.com>
2024-06-30 13:33:45 +0300

    ~

---
+++ HiddenObject/Assets/New Scripts/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText; 
+    [SerializeField] private float timeRemaining = 60;
+    [SerializeField] private bool timerIsRunning = false;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    private void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    public void SetTime(float time)
+    {
+        timeRemaining -= time;
+    }
+
+    private void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
