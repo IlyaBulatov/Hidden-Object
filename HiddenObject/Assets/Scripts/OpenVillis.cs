2e5b23c82284724a6043e9c1e2f79b173f25c9b0
IlyaBulatov <ezgoichik@gmail.com>
2024-06-30 11:14:55 +0500

    Villi's work

---
+++ HiddenObject/Assets/Scripts/OpenVillis.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class OpenVillis : MonoBehaviour
+{
+    [SerializeField] private GameObject View;
+    [SerializeField] private Sprite newSprite;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D objectCollider;
+
+    private void Start()
+    {
+        spriteRenderer = View.GetComponent<SpriteRenderer>();
+        objectCollider = GetComponent<Collider2D>();
+    }
+    private void OnMouseDown()
+    {
+        if (spriteRenderer != null && newSprite != null)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
+        if(objectCollider != null)
+        {
+            objectCollider.enabled = false;
+        }
+    }
+}
